095c3800443aa0fdf96a629304133f12c172f261
IkhsanAdil <adilikhsanw@gmail.com>
2024-08-27 01:06:53 +0700

    Update main.js

--- flowers-for-someone-main/js/main.js
+++ flowers-for-someone-main/js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('TIDUR KOCAK!').split('')
+    const titles = ('JGN NGAMBEK LAGI YA!').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
